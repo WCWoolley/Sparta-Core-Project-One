65493a723c6271de3dc4e8a0fa295e468a35b441
WCWoolley <39518978+WCWoolley@users.noreply.github.com>
2018-06-05 12:14:14 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -96,45 +96,7 @@ $(document).ready(function(){
         $pacman.animate({top: "+=300"}, 1000, "linear");
       }
 
-      // interval = setInterval(function(){
-      // Check ball position
-      // var pacLeft = $pacman.offset().left;
-      // var pacTop = $pacman.offset().top;
-      // var pacRight = pacLeft + $pacman.width();
-      // var pacBottom = pacTop + $pacman.height();
-      //
-      // // Check container position
-      // var wallLeft = $wall.offset().left;
-      // var wallTop = $wall.offset().top;
-      // var wallRight = wallLeft + $wall.width();
-      // var wallBottom = wallTop + $wall.height();
-
-      // When ballRight is greater than containerRight
-      // if (pacRight > wallLeft) {
-      //   $pacman.stop();
-      // }
-
-      // When ballLeft is less than containerRight
-      // if (pacLeft < wallRight) {
-      //   $pacman.stop();
-      // }
-
-      // When ballBottom is greater than containerBottom
-      // if (pacBottom >= wallTop) {
-      //   $pacman.stop();
-      // }
-
-      // When ballTop is less than containerTop
-      // if (pacTop <= wallBottom) {
-      //   $pacman.stop();
-      // }
-      // console.log(pacTop);
-      // console.log(wallBottom);
-      // }, 20)
+      
     }
   }
-
-
-  // console.log($wall.offset().left);
-
 });
