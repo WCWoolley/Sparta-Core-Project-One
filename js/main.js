9bf27c86b4f6a96420b6428978351ba8240840bf
Tech-A60 <tech-a60@Tech-A60s-MacBook-Air.local>
2018-06-05 14:49:39 +0100

    Collisions and New Movement

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   var maze = [
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [3,3,0,3,7,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     [1,2,2,2,2,2,2,2,2,2,2,2,2,1,1,2,2,2,2,2,2,2,2,2,2,2,2,1],
     [1,2,1,1,1,1,2,1,1,1,1,1,2,1,1,2,1,1,1,1,1,2,1,1,1,1,2,1],
@@ -34,104 +34,97 @@ $(document).ready(function(){
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
   ];
 
-  console.log(maze[0].length)
+  var pacman = {
+    x: 14,
+    y: 23
+  }
 
   var board = $('#Game-Window');
   var cellCount = 0;
+  var score = 0;
   var wallLeft = [];
   var wallTop = [];
   var wallRight = [];
   var wallBottom = [];
 
-  for (var i = 0; i < maze.length; i++) {
-    var row = $('<tr class="row"></tr>');
-    for (var j = 0; j < maze[i].length; j++) {
-      if (maze[i][j] === 0) {
-        var col = $(`<td class="col score${cellCount}"></td>`)
-        row.append(col);
-      } else if (maze[i][j] === 1) {
-        var col = $(`<td class="col wall${cellCount}"></td>`).css("background","white")
-        row.append(col);
-      } else if (maze[i][j] === 2) {
-        var col = $(`<td class="col space${cellCount}"><div class="container2"><div class="coin"></div></div></td>`)
-        row.append(col);
-      } else if (maze[i][j] === 3) {
-        var col = $(`<td class="col cage${cellCount}"></td>`).css("background","black")
-        row.append(col);
-      } else if (maze[i][j] === 4) {
-        var col = $(`<td class="col door${cellCount}"></td>`).css("background","black")
-        row.append(col);
-      } else if (maze[i][j] === 5) {
-        var col = $(`<td class="col space${cellCount}"><div class="container1"><div class="pacman"></div></div></td>`).css("background","black")
-        row.append(col);
-      } else if (maze[i][j] === 6) {
-        var col = $(`<td class="col exit${cellCount}"></td>`).css("background","black")
-        row.append(col);
+  function drawMaze(){
+    document.getElementById(`Game-Window`).innerHTML = "";
+    for (var i = 0; i < maze.length; i++) {
+      var row = $('<tr class="row"></tr>');
+      for (var j = 0; j < maze[i].length; j++) {
+        if (maze[i][j] === 0) {
+          var col = $(`<td class="col score1">Score:</td>`).css("border","none").css("color","white")
+          row.append(col);
+        } else if (maze[i][j] === 1) {
+          var col = $(`<td class="col wall${cellCount}"></td>`).css("background","white").css("border-color","black")
+          row.append(col);
+        } else if (maze[i][j] === 2) {
+          var col = $(`<td class="col space${cellCount}"><div class="container2"><div class="coin"></div></div></td>`).css("border","none")
+          row.append(col);
+        } else if (maze[i][j] === 3) {
+          var col = $(`<td class="col cage${cellCount}"></td>`).css("background","black").css("border","none")
+          row.append(col);
+        } else if (maze[i][j] === 4) {
+          var col = $(`<td class="col door${cellCount}"></td>`).css("background","black")
+          row.append(col);
+        } else if (maze[i][j] === 5) {
+          var col = $(`<td class="col space${cellCount}"><div class="container1"><div class="pacman"></div></div></td>`).css("background","black").css("border","none")
+          row.append(col);
+        } else if (maze[i][j] === 6) {
+          var col = $(`<td class="col exit${cellCount}"></td>`).css("background","black").css("border","none")
+          row.append(col);
+        } else if (maze[i][j] === 7) {
+          var col = $(`<td class="col score2">-${score}</td>`).css("border","none").css("color","white")
+          row.append(col);
+        }
+        cellCount++;
       }
-      cellCount++;
+      board.append(row);
     }
-    board.append(row);
   }
 
-  var $pacman = $('.pacman');
-  var interval;
-
-  setInterval(movePlane, 200);
-  var keys = {}
-
-  $(document).keydown(function(e) {
-    keys[e.keyCode] = true;
-  });
-
-  $(document).keyup(function(e) {
-    delete keys[e.keyCode];
-  });
-
-  function movePlane() {
-    for (var direction in keys) {
-      if (!keys.hasOwnProperty(direction)) continue;
-      if (direction == 37) {
-        $pacman.animate({left: "-=300"}, 1000, "linear");
+  document.onkeydown = function(e){
+    if ((e.keyCode === 37)) {
+      if (maze[pacman.y][pacman.x-1] ==6) {
+        maze[pacman.y][pacman.x] = 3;
+        pacman.x = 26;
+        maze[pacman.y][pacman.x] = 5;
+        drawMaze();
+      } else if (maze[pacman.y][pacman.x-1] !==1) {
+        maze[pacman.y][pacman.x] = 3;
+        pacman.x = pacman.x - 1;
+        maze[pacman.y][pacman.x] = 5;
+        drawMaze();
       }
-      if (direction == 38) {
-        $pacman.animate({top: "-=300"}, 1000, "linear");
+    } else if ((e.keyCode === 38)) {
+      if (maze[pacman.y-1][pacman.x] !==1) {
+        maze[pacman.y][pacman.x] = 3;
+        pacman.y = pacman.y - 1;
+        maze[pacman.y][pacman.x] = 5;
+        drawMaze();
       }
-      if (direction == 39) {
-        $pacman.animate({left: "+=300"}, 1000, "linear");
+    } else if ((e.keyCode === 39)) {
+      if (maze[pacman.y][pacman.x+1] ==6) {
+        maze[pacman.y][pacman.x] = 3;
+        pacman.x = 1;
+        maze[pacman.y][pacman.x] = 5;
+        drawMaze();
+      } else if (maze[pacman.y][pacman.x+1] !==1) {
+        maze[pacman.y][pacman.x] = 3;
+        pacman.x = pacman.x + 1;
+        maze[pacman.y][pacman.x] = 5;
+        drawMaze();
       }
-      if (direction == 40) {
-        $pacman.animate({top: "+=300"}, 1000, "linear");
+    } else if ((e.keyCode === 40)) {
+      if (maze[pacman.y+1][pacman.x] !==1) {
+        maze[pacman.y][pacman.x] = 3;
+        pacman.y = pacman.y + 1;
+        maze[pacman.y][pacman.x] = 5;
+        drawMaze();
       }
-
-        interval = setInterval(function(){
-          // Check ball position
-          var pacLeft = $pacman.offset().left;
-          var pacTop = $pacman.offset().top;
-          var pacRight = pacLeft + $pacman.width();
-          var pacBottom = pacTop + $pacman.height();
-
-          // When ballRight is greater than containerRight
-          if (pacRight >= wallLeft[i]) {
-            $pacman.stop();
-          }
-
-          // When ballLeft is less than containerRight
-          if (pacLeft <= wallRight[i]) {
-            $pacman.stop();
-          }
-
-          // When ballBottom is greater than containerBottom
-          if (pacBottom >= wallTop[i]) {
-            $pacman.stop();
-          }
-
-          // When ballTop is less than containerTop
-          if (pacTop <= wallBottom[i]) {
-            $pacman.stop();
-          }
-        }, 20);
-      
     }
-
   }
+
+  drawMaze();
+
 });
